78d1df389f1e70c36e35bec60439a0af150c9c3e
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-09-27 15:26:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b alpha https://github.com/TeamDeeCode/VcVideoPlayer.git /VcVideoPlayer
-cd /VcVideoPlayer
+git clone -b alpha https://github.com/kavinduaj12/mrjokermusicvideo.git /mrjokermusicvideo
+cd /mrjokermusicvideo
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
